ef7d2f865786204212949c56485db6568809439c
Sleepyz4life <mwz_23@msn.com>
2016-06-01 14:21:18 +0200

    Login Functionality

--- BURGAR/app/src/main/java/com/example/sleepyz/burgar/MainActivity.java
+++ BURGAR/app/src/main/java/com/example/sleepyz/burgar/MainActivity.java
@@ -1,45 +1,16 @@
 package com.example.sleepyz.burgar;
 
-        import android.annotation.TargetApi;
-        import android.app.Activity;
-        import android.bluetooth.BluetoothAdapter;
-        import android.bluetooth.BluetoothDevice;
-        import android.bluetooth.BluetoothGatt;
-        import android.bluetooth.BluetoothGattCallback;
-        import android.bluetooth.BluetoothGattCharacteristic;
-        import android.bluetooth.BluetoothGattService;
-        import android.bluetooth.BluetoothManager;
-        import android.bluetooth.BluetoothProfile;
-        import android.bluetooth.le.BluetoothLeScanner;
-        import android.bluetooth.le.ScanCallback;
-        import android.bluetooth.le.ScanFilter;
-        import android.bluetooth.le.ScanResult;
-        import android.bluetooth.le.ScanSettings;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.content.pm.PackageManager;
-        import android.os.Build;
-        import android.os.Bundle;
-        import android.os.Handler;
-        import android.support.v7.app.ActionBarActivity;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Toast;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Bundle;
 
-        import java.util.ArrayList;
-        import java.util.List;
 
 @TargetApi(21)
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void Login(View v) {
-        Intent i = new Intent(this, BluetoothActivity.class);
-        startActivity(i);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ BURGAR/app/src/main/java/com/example/sleepyz/burgar/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.sleepyz.burgar.app;
+
+/**
+ * Created by Sleepyz on 6/1/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://project.cmi.hr.nl/2015_2016/emedia_mt2b_t3/burguard_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://project.cmi.hr.nl/2015_2016/emedia_mt2b_t3/burguard_api/register.php";
+}
