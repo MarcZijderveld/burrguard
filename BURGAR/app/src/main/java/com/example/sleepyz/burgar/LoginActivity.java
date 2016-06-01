900818b4656e45b277ab48dd50e2f8ec247880aa
mentosmenno2 <mentosmenno2@hotmail.com>
2016-06-02 01:55:15 +0200

    aanpassingen

--- BURGAR/app/src/main/java/com/example/sleepyz/burgar/LoginActivity.java
+++ BURGAR/app/src/main/java/com/example/sleepyz/burgar/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -41,6 +42,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
 
         inputEmail = (EditText) findViewById(R.id.email);
