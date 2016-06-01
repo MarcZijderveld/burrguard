1ce00a8e7ff99872af1cf9091ff36baaee26f486
Sleepyz4life <mwz_23@msn.com>
2016-06-01 14:35:50 +0200

    Activities

--- BURGAR/app/src/main/java/com/example/sleepyz/burgar/MainActivity.java
+++ BURGAR/app/src/main/java/com/example/sleepyz/burgar/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.sleepyz.burgar;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 @TargetApi(21)
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
