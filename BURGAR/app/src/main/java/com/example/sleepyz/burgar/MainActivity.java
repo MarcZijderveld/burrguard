0ecfd2cf912c010c216bde28cf3d611ab17529a6
Sleepyz4life <mwz_23@msn.com>
2016-06-02 12:56:59 +0200

    Splash time shorter (2 Secs)

--- BURGAR/app/src/main/java/com/example/sleepyz/burgar/MainActivity.java
+++ BURGAR/app/src/main/java/com/example/sleepyz/burgar/MainActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 @TargetApi(21)
 public class MainActivity extends Activity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
