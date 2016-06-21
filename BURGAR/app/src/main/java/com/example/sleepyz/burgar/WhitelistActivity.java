7719cd527f4379f9a30d5d2b0b20927f6451e72e
DennievanderStarre <dennievanderstarre@hotmail.com>
2016-06-21 15:41:16 +0200

    Working Delete Dialog
    Mede mogelijk gemaakt door...
    
    TIM! TIM! TIM!
    I        I       I
    M     M     M
    !        !       !

--- BURGAR/app/src/main/java/com/example/sleepyz/burgar/WhitelistActivity.java
+++ BURGAR/app/src/main/java/com/example/sleepyz/burgar/WhitelistActivity.java
@@ -1,7 +1,11 @@
 package com.example.sleepyz.burgar;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -43,8 +47,9 @@ public class WhitelistActivity extends AppCompatActivity {
         lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                FriendlistArray.remove(position);
-                adapter.notifyDataSetChanged();
+
+                createDialog(position);
+
                 return true;
             }
         });
@@ -56,7 +61,25 @@ public class WhitelistActivity extends AppCompatActivity {
                 adapter.notifyDataSetChanged();
             }
         });
-
     }
 
+    public Dialog createDialog(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(WhitelistActivity.this);
+        builder.setMessage(R.string.delete_dialog)
+                .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        FriendlistArray.remove(position);
+                        adapter.notifyDataSetChanged();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        Log.d("Het werkt", "Echt waar");// Create the AlertDialog object and return it
+        builder.create();
+        return builder.show();
+
+    }
 }
